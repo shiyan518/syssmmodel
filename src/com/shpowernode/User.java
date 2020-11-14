0cc999a132e1d29e647b156beff89f9092bb2505
shiyi <1220905702@qq.com>
2020-11-14 16:21:06 +0800

    first commit

---
+++ src/com/shpowernode/User.java
@@ -0,0 +1,17 @@
+package com.shpowernode;
+
+/**
+ * @ProjectName: gitmodel
+ * @Package: com.shpowernode
+ * @Description: java类作用描述
+ * @Author: 石岩
+ * @CreateDate: 2020/11/14 16:19
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class User {
+
+    private String name;
+
+}
